dc82d80e1903f6d89d73da4612cc9ae08b0613fd
Frederik Bußmann <frederik@bussmann.io>
2026-06-04 15:01:59 +0200

    feat: add config file support
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
-import { writeFile } from 'node:fs/promises'
+import type { ProbeConfig } from 'autodisco'
 
+import { writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import process from 'node:process'
 import discover from 'autodisco'
+import { loadConfig } from 'c12'
 import { defineCommand, runMain } from 'citty'
+import defu from 'defu'
 import { config as loadEnv } from 'dotenv'
+
 import { buildBaseUrl, buildHeaders, createClient } from '../client'
 
 const { parsed } = loadEnv({ quiet: true })
@@ -14,6 +18,11 @@ const env = parsed ?? process.env
 const COLLECTION = Object.entries(env).find(([key]) => key.endsWith('BATTERYINCLUDED_COLLECTION'))?.[1]
 const API_KEY = Object.entries(env).find(([key]) => key.endsWith('BATTERYINCLUDED_API_KEY'))?.[1]
 
+/**
+ * Builds the content of the augmentation file to merge generated types with the batteryincluded package types.
+ *
+ * @returns The content of the augmentation file as a string.
+ */
 function buildAugmentationFile() {
   return [
     'import type { paths as generatedPaths, components as generatedComponents } from \'./openapi/types\'',
@@ -98,6 +107,19 @@ const command = defineCommand({
       throw new Error('API key is required. Please specify it using the --api-key flag or set the BATTERYINCLUDED_API_KEY environment variable.')
     }
 
+    const cwd = process.cwd()
+
+    // Resolved config from batteryincluded rc file if it exists
+    const { config: resolved } = await loadConfig<Record<string, ProbeConfig | ProbeConfig[]>>({
+      name: 'batteryincluded',
+      cwd,
+      rcFile: 'batteryincluded-rc',
+      globalRc: false,
+      dotenv: false,
+    })
+
+    let config: Record<string, ProbeConfig | ProbeConfig[]> = resolved ?? {}
+
     const suggestQuery = args.suggestQuery ?? 'a'
 
     let recommendationId = args.recommendationId
@@ -105,13 +127,38 @@ const command = defineCommand({
     if (!recommendationId) {
       const client = createClient<BrowseClientPaths>({ collection: args.collection, apiKey: args.apiKey })
 
-      recommendationId = await client.GET('/browse').then(({ data }) => data?.hits?.[0]?.document?.id)
+      recommendationId = await client.GET('/browse', { params: { query: { per_page: 1 } } }).then(({ data }) => data?.hits?.[0]?.document?.id)
     }
 
     if (!recommendationId) {
       throw new Error('No document found in the collection to use for generating recommendation types. Please specify a document ID using the --recommendation-id flag or ensure that the collection contains at least one document.')
     }
 
+    // Default config with required parameters if no other config provided
+    config = defu(config, {
+      '/browse': {
+        query: {
+          page: 1,
+          per_page: 10,
+        },
+      },
+
+      '/highlights': {},
+
+      '/suggest': {
+        query: {
+          q: suggestQuery,
+        },
+      },
+
+      '/recommendations': {
+        query: {
+          id: recommendationId,
+        },
+      },
+    })
+
+    // Run discovery to generate OpenAPI schema and types based on the provided config
     await discover({
       baseUrl: buildBaseUrl(args.collection),
       headers: buildHeaders(args.apiKey),
@@ -127,30 +174,12 @@ const command = defineCommand({
 
       probes: {
         get: {
-          '/browse': {
-            query: {
-              page: 1,
-              per_page: 100,
-            },
-          },
-
-          '/highlights': {},
-
-          '/suggest': {
-            query: {
-              q: suggestQuery,
-            },
-          },
-
-          '/recommendations': {
-            query: {
-              id: recommendationId,
-            },
-          },
+          ...config,
         },
       },
     })
 
+    // Write augmentation file to merge generated types with the batteryincluded package types
     await writeFile(join(args.outputDir, 'index.d.ts'), buildAugmentationFile(), 'utf8')
   },
 })
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import type { ProbeConfig } from 'autodisco'
+
+export function defineCodegenConfig(config: Record<string, ProbeConfig | ProbeConfig[]>): Record<string, ProbeConfig | ProbeConfig[]> {
+  return config
+}
